f8c83ba9145292b3eead95b59b02ec83f5f77037
Ayush Amin <ayush.amin2022@vitstudent.ac.in>
2024-03-02 09:15:15 +0530

    added auth in graphql

--- clients/api.ts
+++ clients/api.ts
@@ -4,6 +4,6 @@ const isClient = typeof window !== "undefined"
 
 export const graphqlClient = new GraphQLClient("http://localhost:8000/graphql" , {
     headers: () => ({
-        Authorization: isClient? `Bearer ${window.localStorage.getItem('__twitter_token')}` : undefined,
+        Authorization: isClient? `Bearer ${window.localStorage.getItem('__twitter_token')}` : "",
     })
 });
